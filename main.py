501458ea719addc256d5fc186f34c63760b4592d
Nada Shaala <nadashaala@.ecole-hexagone.com>
2025-02-10 11:32:50 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class Livre:
+
+    def __init__(self):
+        self.__title = 'title'
+        self.__author = 'author'
+        self.__content = 'content'
+
+
+
+
+def process_trucs():
+    pass
+
+
+if __name__ == '__main__':
+    input_user = input('Que voulez-vous faire? ')
+    process_trucs()
